f90b207ea89f16c835f0b4304e5eb0b6af03e4cd
rripamonti <rripamonti@studenti.uninsubria.it>
2020-04-01 15:19:57 +0200

    Add code for custom anon, little refactor, but code is still ugly ^^

--- src/insubria/MainMasterThesis.java
+++ src/insubria/MainMasterThesis.java
@@ -1,12 +1,12 @@
+package insubria;
 
+import insubria.Utils.TableSupport;
 import org.deidentifier.arx.*;
 import org.deidentifier.arx.criteria.KAnonymity;
 import org.hsqldb.jdbc.JDBCStatement;
-import org.hsqldb.util.DatabaseManagerSwing;
 
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -36,35 +36,22 @@ public class MainMasterThesis{
         Connection c = DriverManager.getConnection("jdbc:hsqldb:file:data\\test", "SA", "");
         System.out.println("Connected");
         statement = (JDBCStatement) c.createStatement();
-        /*
-        importAdultDataset();
 
-        String query = "select maritalStatus, sex, AVG(age) as AVG_AGE, count(sex) as num from adult group by maritalStatus, sex";
-        String fileName = "avg_age_count_sex_gb_maritalstatus_sex";
-        printResultToFile(statement.executeQuery("explain json minimal for "+query),fileName,".json");
-        printResultToFile(statement.executeQuery(query),fileName,".csv");
+        //importAdultDataset();
+
+        //check method to change options and param
+        //anonymizeAdultWithFlash();
+        //importAnonAdultDatasetToDatabase();
+        //generateAnalysisOverAdultTables();
 
         c.close();
-        mondrian();
-        int[] k_array = new int[]{5,10,15,20};
-        double[] s_array = new double[]{0.0,0.04,0.08,0.12,0.16,0.20};
-        for(int k:k_array) {
-            for (double s : s_array) {
-                flashAdult(k, s);
-            }
-        }*/
-        /*
-        int[] k_array = new int[]{5,10,15,20};
-        int[] s_array = new int[]{0,4,8,12,16,20};
-        for(int k:k_array) {
-            importAnonAdultDataset("adult_mondrian_anon_"+k+".csv","adult_mondrian_"+k);
-            for (int s : s_array) {
-                importAnonAdultDataset("adult_flash_anon_"+k+"_"+s+"%.csv","adult_flash_"+k+"_"+s);
-                //System.out.println("drop table "+"adult_flash_"+k+"_"+s);
-            }
-        }*/
+
+    }
+
+    private static void generateAnalysisOverAdultTables() throws SQLException, IOException {
         int[] k_array = new int[]{5,10,15,20};
         int[] sup_array = new int[]{0,4,8,12,16,20};
+        String[] a_array = new String[]{"Age","Workclass","SalaryClass","CombinedWorkAgeSalary"};
         String[] cond_array = new String[]{" where LOWER(workclass) = 'private';workclass_eq_private"," where age_min < 40;age_lt_40"," where income = '<=50K';income_eq_lt50k"};
         for(String s:cond_array) {
             String[] values = s.split(";");
@@ -81,22 +68,58 @@ public class MainMasterThesis{
             for (int k : k_array) {
                 fileName = "count_filter_"+values[1]+"_" + "adult_mondrian_" + k;
                 query = "select '"+fileName+"' as type,count(*) as num from " + "adult_mondrian_" + k + values[0];
-                printResultToFile(statement.executeQuery("explain json minimal for " + query), fileName, ".json");
+                //printResultToFile(statement.executeQuery("explain json minimal for " + query), fileName, ".json");
                 tableSupport.addRows(statement.executeQuery(query), fileName, true);
+
+                fileName = "count_filter_"+values[1]+"_" + "adult_OLA_" + k;
+                query = "select '"+fileName+"' as type,count(*) as num from " + "adult_OLA_" + k + values[0];
+                //printResultToFile(statement.executeQuery("explain json minimal for " + query), fileName, ".json");
+                tableSupport.addRows(statement.executeQuery(query), fileName, true);
+
                 for (int sup : sup_array) {
                     fileName = "count_filter_"+values[1]+"_" + "adult_flash_" + k + "_" + sup;
                     query = "select '"+fileName+"' as type,count(*) as num from " + "adult_flash_" + k + "_" + sup + values[0];
-                    printResultToFile(statement.executeQuery("explain json minimal for " + query), fileName, ".json");
+                    //printResultToFile(statement.executeQuery("explain json minimal for " + query), fileName, ".json");
                     tableSupport.addRows(statement.executeQuery(query), fileName, true);
+                    for (String att : a_array) {
+                        fileName = "count_filter_"+values[1]+"_" + "adult_flash_" + k + "_" + sup + "_" + att;
+                        query = "select '"+fileName+"' as type,count(*) as num from " + "adult_flash_" + k + "_" + sup + "_" + att + values[0];
+                        //printResultToFile(statement.executeQuery("explain json minimal for " + query), fileName, ".json");
+                        tableSupport.addRows(statement.executeQuery(query), fileName, true);
+                    }
                 }
             }
             tableSupport.printToFile();
         }
-        c.close();
-
     }
 
-    private static void importAnonAdultDataset(String filename, String tableName) throws SQLException, IOException {
+    private static void importAnonAdultDatasetToDatabase() throws SQLException, IOException {
+        int[] k_array = new int[]{5,10,15,20};
+        int[] s_array = new int[]{0,4,8,12,16,20};
+        String[] a_array = new String[]{"Age","Workclass","SalaryClass","CombinedWorkAgeSalary"};
+        for(int k:k_array) {
+            importAnonAdultDataset("adult_OLA_anon_"+k+".csv","adult_OLA_"+k,",");
+            importAnonAdultDataset("adult_mondrian_anon_"+k+".csv","adult_mondrian_"+k,";");
+            for (int s : s_array) {
+                importAnonAdultDataset("adult_flash_anon_"+k+"_"+s+"%.csv","adult_flash_"+k+"_"+s,";");
+                for (String att : a_array) {
+                    importAnonAdultDataset("adult_flash_anon_" + k + "_" + s + "%_"+att+".csv", "adult_flash_" + k + "_" + s+"_"+att, ";");
+                }
+            }
+        }
+    }
+
+    private static void anonymizeAdultWithFlash() throws IOException {
+        int[] k_array = new int[]{5,10,15,20};
+        double[] s_array = new double[]{0.0,0.04,0.08,0.12,0.16,0.20};
+        for(int k:k_array) {
+            for (double s : s_array) {
+                flashAdult(k, s,"adult_flash_anon_"+k+"_"+(int)(s*100)+"%_CombinedWorkAgeSalary",new String[]{"workclass","age","salary-class"},0);
+            }
+        }
+    }
+
+    private static void importAnonAdultDataset(String filename, String tableName, String split) throws SQLException, IOException {
         statement.execute("create table if not exists "+tableName+"(age VARCHAR(50), age_min int, age_max int,"
                 + "workclass VARCHAR(50), " + "fnlwgt int, "
                 + "education VARCHAR(50), " + "educationNum int, "
@@ -107,24 +130,26 @@ public class MainMasterThesis{
                 + "nativeCountry VARCHAR(50), " + "income VARCHAR(50))");
         ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM "+tableName);
         resultSet.next();
-        //System.out.println(resultSet.getInt(1));
+        System.out.println(tableName + " " + resultSet.getInt(1));
         if (resultSet.getInt(1)==0) {
-
-            Stream st = Files.lines(Paths.get("./data/anonymized/"+filename)).skip(1);
-            Object[] strings = st.toArray();
-            for(Object o:strings){
-                addAnonAdult(o,tableName);
+            try {
+                Stream st = Files.lines(Paths.get("./data/anonymized/" + filename)).skip(1);
+                Object[] strings = st.toArray();
+                for (Object o : strings) {
+                    addAnonAdult(o, tableName, split);
+                }
+            } catch (IOException e) {
+                return;
             }
         }
     }
 
-    private static void addAnonAdult(Object o,String tableName){
+    private static void addAnonAdult(Object o,String tableName,String split){
         String s = (String) o;
         s = s.replaceAll("\"","'");
-        String[] values = s.split(";");
+        String[] values = s.split(split);
         //check if row is suppressed
         if (Arrays.stream(values).filter(x->x.equals("*")).count()==9){
-            System.out.println("Suppressed");
             return;
         }
         values = Arrays.stream(values).map(x->x.equals("*")?"null":x).toArray(String[]::new);
@@ -203,10 +228,12 @@ public class MainMasterThesis{
                 + "sex VARCHAR(50), " + "capitalGain int, "
                 + "capitalLoss int, " + "hoursPerWeek int, "
                 + "nativeCountry VARCHAR(50), " + "income VARCHAR(50))");
+
         ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM adult");
         resultSet.next();
+        System.out.println(resultSet.getInt(1));
         if (resultSet.getInt(1)==0) {
-            Stream st = Files.lines(Paths.get("./data/dataset/adult.csv"));
+            Stream st = Files.lines(Paths.get("./data/dataset/adult_clear.csv")).skip(1);
             st.forEach(MainMasterThesis::addAdult);
         }
     }
@@ -253,7 +280,7 @@ public class MainMasterThesis{
         //System.out.println(algo.getResults().get(0).get(0).getValue(2) +" "+algo.getResults().get(0).get(1).getValue(2));
     }
 */
-    private  static void flashAdult(int k, double suppression) throws IOException {
+    private  static void flashAdult(int k, double suppression, String filename,String[] attribute, int maxLevel) throws IOException {
         Data data = Data.create("./data/dataset/adult_clear.csv", StandardCharsets.UTF_8, ',');
 
         // Define input files
@@ -274,7 +301,12 @@ public class MainMasterThesis{
         data.getDefinition().setAttributeType("hours-per-week",AttributeType.INSENSITIVE_ATTRIBUTE);
 
         // set the minimal/maximal generalization height
-        //data.getDefinition().setMaximumGeneralization("age", 3);
+        if (attribute!=null) {
+            for (String att: attribute
+                 ) {
+                data.getDefinition().setMaximumGeneralization(att, maxLevel);
+            }
+        }
         //data.getDefinition().setMaximumGeneralization("education", 2);
 
         // Create an instance of the anonymizer
@@ -286,15 +318,17 @@ public class MainMasterThesis{
         config.setSuppressionLimit(suppression);
         ARXResult result = anonymizer.anonymize(data, config);
 
-        String filename = "adult_flash_anon_"+k+"_"+(int)(suppression*100)+"%";
+        //String filename = "adult_flash_anon_"+k+"_"+(int)(suppression*100)+"%";
 
         // Print info
         printResult(result, data, filename);
 
         // Write results
-        System.out.print(" - Writing data...");
-        result.getOutput(false).save("data/anonymized/"+filename+".csv", ';');
-        System.out.println("Done!");
+        if (result.getGlobalOptimum()!=null) {
+            System.out.print(" - Writing data...");
+            result.getOutput(false).save("data/anonymized/" + filename + ".csv", ';');
+            System.out.println("Done!");
+        }
     }
 
     protected static void printResult(final ARXResult result, final Data data, String fileName) throws IOException {
@@ -312,6 +346,12 @@ public class MainMasterThesis{
         final List<String> qis = new ArrayList<String>(data.getDefinition().getQuasiIdentifyingAttributes());
 
         if (optimum == null) {
+            File dir = new File("./data/anonymized");
+            dir.mkdirs();
+            File file = new File(dir,fileName+"_error.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream(file,true);
+
+            fileOutputStream.write("Impossible to anonymize...".getBytes());
             return ;
         }
 
--- src/insubria/Utils/TableSupport.java
+++ src/insubria/Utils/TableSupport.java
@@ -1,3 +1,5 @@
+package insubria.Utils;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
